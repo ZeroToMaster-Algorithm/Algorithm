058b532a7a8c5971c9a04320b2f3700bbdcbd4b1
Sunro Lee <tjsfhdhkd@naver.com>
2024-04-30 16:05:37 +0900

    2007년_1924

---
+++ BOJ/Sunro/2007년_1924.java
@@ -0,0 +1,31 @@
+package Simulation.silver;
+
+import java.util.*;
+
+public class 2007년_1924 {
+    static int[] cal = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+    static String[] week = new String[]{"MON","TUE","WED","THU","FRI","SAT","SUN"};
+    static int month, day;
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        month = sc.nextInt();
+        day = sc.nextInt();
+        int sum =0;
+
+        if (month >= 2) {
+            for (int i = 0; i < month-1; i++) {
+                sum += cal[i];
+            }
+            sum+=day;
+        }else{
+            sum +=day;
+        }
+
+        int result = (sum-1)%7;
+        System.out.println(week[result]);
+
+
+
+    }
+}
